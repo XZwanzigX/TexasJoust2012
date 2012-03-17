cf0bb7cb45c2c91957490e247bbbd38221276f29
Foxfire <foxfire@evilcupcakes.net>
2012-03-16 19:27:06 -0500

    Fixed up directions page

--- directions.php
+++ directions.php
@@ -4,16 +4,23 @@
         <link rel="stylesheet" href="css/structure.css" type="text/css"/>
         <link rel="stylesheet" href="css/navOther.css" type="text/css"/>
         <link rel="stylesheet" href="css/textStyles.css" type="text/css"/>
+        <link rel="stylesheet" href="css/dropMenu.css" type="text/css"/>
+
+        <script src="js/jquery-1.2.6.min.js"></script>
+        <script src="js/dropMenu.js"></script>
 	</head>
 
 	<body class="other-page-body">
         <img src="img/template/blank.png" class="bottom-page"/>
 		<div id="content" class="other-page-text">
-            <?php include('template/nav.php');?>
-            <p>Lysts on the Lake is held at the Taylor Rodeo Association arena in Taylor Texas.  This facility is located on the North side of Taylor, West of state highway 95, and North of county road 387 (known locally as Carlos Parker Blvd. or the Loop) at 210 Carlos G Parker Blvd NW, Taylor, Texas.</p>
+            <?php include('template/ddNav.php');?>
 
-            <p>Taylor is located on state highway 79, just 18 miles East of Round Rock. You can also find Taylor by taking state highway 95 South of Temple 40 miles, or North of Elgin 18 miles.</p>
-            <p><iframe width="800" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?hl=en&amp;q=210+Carlos+G+Parker+Blvd+NW,Taylor,+Texas&amp;ie=UTF8&amp;hq=&amp;hnear=210+Carlos+G+Parker+Blvd+NW,+Taylor,+Texas+76574&amp;gl=us&amp;t=h&amp;z=14&amp;ll=30.600328,-97.421854&amp;output=embed"></iframe><br /><small><a href="http://maps.google.com/maps?hl=en&amp;q=210+Carlos+G+Parker+Blvd+NW,Taylor,+Texas&amp;ie=UTF8&amp;hq=&amp;hnear=210+Carlos+G+Parker+Blvd+NW,+Taylor,+Texas+76574&amp;gl=us&amp;t=h&amp;z=14&amp;ll=30.600328,-97.421854&amp;source=embed" style="color:#0000FF;text-align:left">View Larger Map</a></small></p>
-		</div>
+            <div class="text-container">
+                <p>Lysts on the Lake is held at the Taylor Rodeo Association arena in Taylor Texas.  This facility is located on the North side of Taylor, West of state highway 95, and North of county road 387 (known locally as Carlos Parker Blvd. or the Loop) at 210 Carlos G Parker Blvd NW, Taylor, Texas.</p>
+
+                <p>Taylor is located on state highway 79, just 18 miles East of Round Rock. You can also find Taylor by taking state highway 95 South of Temple 40 miles, or North of Elgin 18 miles.</p>
+                <p><iframe width="800" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?hl=en&amp;q=210+Carlos+G+Parker+Blvd+NW,Taylor,+Texas&amp;ie=UTF8&amp;hq=&amp;hnear=210+Carlos+G+Parker+Blvd+NW,+Taylor,+Texas+76574&amp;gl=us&amp;t=h&amp;z=14&amp;ll=30.600328,-97.421854&amp;output=embed"></iframe><br /><small><a href="http://maps.google.com/maps?hl=en&amp;q=210+Carlos+G+Parker+Blvd+NW,Taylor,+Texas&amp;ie=UTF8&amp;hq=&amp;hnear=210+Carlos+G+Parker+Blvd+NW,+Taylor,+Texas+76574&amp;gl=us&amp;t=h&amp;z=14&amp;ll=30.600328,-97.421854&amp;source=embed" style="color:#0000FF;text-align:left">View Larger Map</a></small></p>
+            </div>
+        </div>
 	</body>
 </html>
