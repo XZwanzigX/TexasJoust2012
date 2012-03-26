a9ae12e7549888f2377caf0228436802091397d9
Foxfire <foxfire@evilcupcakes.net>
2012-03-26 08:00:15 -0500

    Altered links on drop nav for photo and video galleries.

--- schedule.php
+++ schedule.php
@@ -5,10 +5,137 @@
         <link rel="stylesheet" href="css/textStyles.css" type="text/css"/>
 	    <link rel="stylesheet" href="css/navOther.css" type="text/css"/>
         <link rel="stylesheet" href="css/dropMenu.css" type="text/css"/>
+        <link rel="stylesheet" href="css/scheduleStyles.css" type="text/css"/>
 
         <script type="text/javascript" src="js/jquery-1.2.6.min.js"></script>
         <script type="text/javascript" src="js/dropMenu.js"></script>
 	</head>
 
-	<?php include('phpIncludes/comingSoon.php'); ?>
+	<?php include('template/otherPageBegin.php'); ?>
+            <table>
+                <tr class="schedule-day">
+                    <td>&nbsp</td>
+                    <td colspan="2">Friday 5/11</td>
+                    <td colspan="2">Sat. 5/12</td>
+                    <td colspan="2">Sunday 5/13</td>
+                </tr>
+                <tr>
+                    <td>&nbsp;</td>
+                    <td class="eq-field">Eq. Field</td>
+                    <td class="foot-field">Foot Field</td>
+                    <td class="eq-field">Eq. Field</td>
+                    <td class="foot-field">Foot Field</td>
+                    <td class="eq-field">Eq. Field</td>
+                    <td class="foot-field">Foot Field</td>
+                </tr>
+                <tr>
+                    <td>8:00a</td>
+                    <td colspan="6">Breakfast</td>
+                </tr>
+                <tr>
+                    <td>9:00a</td>
+                    <td colspan="2" rowspan="3">&nbsp;</td>
+                    <td colspan="4">Tack and armour up</td>
+                </tr>
+                <tr>
+                    <td>9:30a</td>
+                    <td colspan="4">Gate Opens</td>
+                </tr>
+                <tr>
+                    <td>10:00a</td>
+                    <td rowspan="3">Procession and Melee a'Cheval</td>
+                    <td rowspan="5">Blossfechten longsword Tournament</td>
+                    <td rowspan="3">Procession and Skill at Arms Competition #2</td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td>10:30a</td>
+                    <td colspan="2" rowspan="3">&nbsp;</td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td>11:00a</td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td>11:30a</td>
+                    <td>EAFK Presentation</td>
+                    <td>EAFK Presentation</td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td>12:00n</td>
+                    <td colspan="2">Gate Opens</td>
+                    <td rowspan="2">Joust #4</td>
+                    <td rowspan="2">Joust #7</td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td>12:30p</td>
+                    <td rowspan="2">Procession and Skill at Arms Competition #1</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td>1:30p</td>
+                    <td>&nbsp;</td>
+                    <td rowspan="2">&nbsp;</td>
+                    <td rowspan="2">Armored Poleaxe Tournament</td>
+                    <td rowspan="2">&nbsp;</td>
+                    <td rowspan="2">Armoured Deed at Arms</td>
+                </tr>
+                <tr>
+                    <td>2:00p</td>
+                    <td colspan="2">EAFK Presentation</td>
+                </tr>
+                <tr>
+                    <td>2:30p</td>
+                    <td>Joust #1</td>
+                    <td>&nbsp;</td>
+                    <td>Joust #5</td>
+                    <td>&nbsp;</td>
+                    <td>Joust #8</td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td>4:00p</td>
+                    <td>&nbsp;</td>
+                    <td>Armoured Deed at Arms</td>
+                    <td>&nbsp</td>
+                    <td>Finals for Blossfechten</td>
+                    <td>&nbsp</td>
+                    <td>Armoured Challenge at Spear</td>
+                </tr>
+                <tr>
+                    <td>5:00p</td>
+                    <td>Joust #2</td>
+                    <td>&nbsp;</td>
+                    <td rowspan="2">Joust #6</td>
+                    <td rowspan="2">&nbsp;</td>
+                    <td rowspan="2">Joust #9</td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td>6:30p</td>
+                    <td colspan="2" rowspan="2">Novice Mounted Melee</td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td>7:00p</td>
+                    <td rowspan="4">Site Closes</td>
+                    <td colspan="4">&nbsp;</td>
+                </tr>
+                <tr>
+                    <td>7:30p</td>
+                    <td>Joust #3</td>
+                    <td>&nbsp;</td>
+                    <td colspan="4" rowspan="2">&nbsp;</td>
+                </tr>
+                <tr>
+                    <td>9:00p</td>
+                    <td colspan="2">Site Closes</td>
+                </tr>
+            </table>
+    <?php include('template/otherPageEnd.php'); ?>
 </html>
