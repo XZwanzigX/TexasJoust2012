c5d67c91a857ba7b748056e3267f5cd77d002fad
Foxfire <foxfire@evilcupcakes.net>
2012-03-27 17:17:12 -0500

    Made sure that all external nav links will open in new window.

--- template/ddNav.php
+++ template/ddNav.php
@@ -8,20 +8,20 @@
     <li><a href="#"  onmouseover="document.aboutButton.src='img/template/nav/aboutGlow.png'" onmouseout="document.aboutButton.src='img/template/nav/about.png'">
         <img src="img/template/nav/about.png" name="aboutButton"></a>
         <ul>
-            <li><a href="http://www.taylorrodeo.com">The Venue</a></li>
+            <li><a href="http://www.taylorrodeo.com" target="_blank">The Venue</a></li>
             <li><a href="joust.php">The Joust & Melee a'Cheval</a></li>
             <li><a href="skillAtArms.php">Skill at Arms</a></li>
             <li><a href="competitors.php">The Competitors</a></li>
             <li><a href="schedule.php">2012 Schedule of Events</a></li>
-            <li><a href="http://www.chivalricmartialartsinternational.com" target="_new">Chivalric Marital Arts International Symposium</a></li>
+            <li><a href="http://www.chivalricmartialartsinternational.com" target="_blank">Chivalric Marital Arts International Symposium</a></li>
         </ul>
     </li>
     <li>
         <a href="#"  onmouseover="document.galleriesButton.src='img/template/nav/galleriesGlow.png'" onmouseout="document.galleriesButton.src='img/template/nav/galleries.png'">
             <img src="img/template/nav/galleries.png" name="galleriesButton"></a>
         <ul>
-            <li><a href="http://azulox.zenfolio.com/joust2011" target="_new">Photos</a></li>
-            <li><a href="http://www.youtube.com/aplaisanceltd" target="_new">Videos</a></li>
+            <li><a href="http://azulox.zenfolio.com/joust2011" target="_blank">Photos</a></li>
+            <li><a href="http://www.youtube.com/aplaisanceltd" target="_blank">Videos</a></li>
         </ul>
     </li>
     <li>
@@ -31,7 +31,7 @@
             <li><a href="joustingHistory.php">History of Jousting</a></li>
             <li><a href="links.php">Special Links</a></li>
             <li><a href="pressRelease.php">Press Release</a></li>
-            <li><a href="http://www.aplaisance.com" target="_new">a'Plaisance Ltd.</a></li>
+            <li><a href="http://www.aplaisance.com" target="_blank">a'Plaisance Ltd.</a></li>
             <li><a href="2011.php">Lysts on the Lake 2011</a></li>
         </ul>
     </li>
@@ -39,11 +39,11 @@
         <a href="#" onmouseover="document.sponsorsButton.src='img/template/nav/sponsorsGlow.png'" onmouseout="document.sponsorsButton.src='img/template/nav/sponsors.png'">
             <img src="img/template/nav/sponsors.png" name="sponsorsButton"></a>
         <ul>
-            <li><a href="http://eafk.org">Early Act First Knight</a></li>
-            <li><a href="http://www.azulox.com">AzulOx Photography</a></li>
-            <li><a href="http://www.darkwoodarmory.com">Darkwood Armory</a></li>
-            <li><a href="http://www.revivalclothing.com">Revival Clothing</a></li>
-            <li><a href="http://www.knightsofiron.com/">Knights of Iron</a></li>
+            <li><a href="http://eafk.org" target="_blank">Early Act First Knight</a></li>
+            <li><a href="http://www.azulox.com" target="_blank">AzulOx Photography</a></li>
+            <li><a href="http://www.darkwoodarmory.com" target="_blank">Darkwood Armory</a></li>
+            <li><a href="http://www.revivalclothing.com" target="_blank">Revival Clothing</a></li>
+            <li><a href="http://www.knightsofiron.com/" target="_blank">Knights of Iron</a></li>
         </ul>
     </li>
     <li>
